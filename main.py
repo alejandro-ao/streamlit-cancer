08de748da5836b9dff1dcd9b6728a9e24015d3e1
Alejandro <aavila0707@gmail.com>
2023-03-28 20:59:03 +0200

    refactor: sidebar sliders

--- main.py
+++ main.py
@@ -105,155 +105,28 @@ def create_input_form(data):
     import streamlit as st
 
     st.sidebar.header("Cell Nuclei Details")
-    # Add a form to input the features
-    radius = st.sidebar.slider(
-        "Radius (mean)", float(data.radius_mean.min()), float(data.radius_mean.max()),
-        float(data.radius_mean.mean())
-    )
-    texture = st.sidebar.slider(
-        "Texture (mean)", float(data.texture_mean.min()), float(data.texture_mean.max()),
-        float(data.texture_mean.mean())
-    )
-    perimeter = st.sidebar.slider(
-        "Perimeter (mean)", float(data.perimeter_mean.min()), float(data.perimeter_mean.max()),
-        float(data.perimeter_mean.mean())
-    )
-    area = st.sidebar.slider(
-        "Area (mean)", float(data.area_mean.min()), float(data.area_mean.max()),
-        float(data.area_mean.mean())
-    )
-    smoothness = st.sidebar.slider(
-        "Smoothness (mean)", float(data.smoothness_mean.min()), float(data.smoothness_mean.max()),
-        float(data.smoothness_mean.mean())
-    )
-    compactness = st.sidebar.slider(
-        "Compactness (mean)", float(data.compactness_mean.min()),
-        float(data.compactness_mean.max()), float(data.compactness_mean.mean())
-    )
-    concavity = st.sidebar.slider(
-        "Concavity (mean)", float(data.concavity_mean.min()), float(data.concavity_mean.max()),
-        float(data.concavity_mean.mean())
-    )
-    concave_points = st.sidebar.slider(
-        "Concave points (mean)", float(data['concave points_mean'].min()),
-        float(data['concave points_mean'].max()), float(data['concave points_mean'].mean())
-    )
-    symmetry = st.sidebar.slider(
-        "Symmetry (mean)", float(data.symmetry_mean.min()), float(data.symmetry_mean.max()),
-        float(data.symmetry_mean.mean())
-    )
-    fractal_dimension = st.sidebar.slider(
-        "Fractal dimension (mean)", float(data.fractal_dimension_mean.min()),
-        float(data.fractal_dimension_mean.max()), float(data.fractal_dimension_mean.mean())
-    )
-    radius_se = st.sidebar.slider(
-        "Radius (se)", float(data.radius_se.min()), float(data.radius_se.max()), float(data.radius_se.mean())
-    )
-    texture_se = st.sidebar.slider(
-        "Texture (se)", float(data.texture_se.min()), float(data.texture_se.max()), float(data.texture_se.mean())
-    )
-    perimeter_se = st.sidebar.slider(
-        "Perimeter (se)", float(data.perimeter_se.min()), float(data.perimeter_se.max()),
-        float(data.perimeter_se.mean())
-    )
-    area_se = st.sidebar.slider(
-        "Area (se)", float(data.area_se.min()), float(data.area_se.max()), float(data.area_se.mean())
-        )
-    smoothness_se = st.sidebar.slider(
-        "Smoothness (se)", float(data.smoothness_se.min()), float(data.smoothness_se.max()),
-        float(data.smoothness_se.mean())
-    )
-    compactness_se = st.sidebar.slider(
-        "Compactness (se)", float(data.compactness_se.min()), float(data.compactness_se.max()),
-        float(data.compactness_se.mean())
-    )
-    concavity_se = st.sidebar.slider(
-        "Concavity (se)", float(data.concavity_se.min()), float(data.concavity_se.max()),
-        float(data.concavity_se.mean())
-    )
-    concave_points_se = st.sidebar.slider(
-        "Concave points (se)", float(data['concave points_se'].min()), float(data['concave points_se'].max()),
-        float(data['concave points_se'].mean())
-    )
-    symmetry_se = st.sidebar.slider(
-        "Symmetry (se)", float(data.symmetry_se.min()), float(data.symmetry_se.max()), float(data.symmetry_se.mean())
-    )
-    fractal_dimension_se = st.sidebar.slider(
-        "Fractal dimension (se)", float(data.fractal_dimension_se.min()), float(data.fractal_dimension_se.max()),
-        float(data.fractal_dimension_se.mean())
-    )
-    radius_worst = st.sidebar.slider(
-        "Radius (worst)", float(data.radius_worst.min()), float(data.radius_worst.max()),
-        float(data.radius_worst.mean())
-    )
-    texture_worst = st.sidebar.slider(
-        "Texture (worst)", float(data.texture_worst.min()), float(data.texture_worst.max()),
-        float(data.texture_worst.mean())
-    )
-    perimeter_worst = st.sidebar.slider(
-        "Perimeter (worst)", float(data.perimeter_worst.min()), float(data.perimeter_worst.max()),
-        float(data.perimeter_worst.mean())
-    )
-    area_worst = st.sidebar.slider(
-        "Area (worst)", float(data.area_worst.min()), float(data.area_worst.max()), float(data.area_worst.mean())
-    )
-    smoothness_worst = st.sidebar.slider(
-        "Smoothness (worst)", float(data.smoothness_worst.min()), float(data.smoothness_worst.max()),
-        float(data.smoothness_worst.mean())
-    )
-    compactness_worst = st.sidebar.slider(
-        "Compactness (worst)", float(data.compactness_worst.min()), float(data.compactness_worst.max()),
-        float(data.compactness_worst.mean())
-    )
-    concavity_worst = st.sidebar.slider(
-        "Concavity (worst)", float(data.concavity_worst.min()), float(data.concavity_worst.max()),
-        float(data.concavity_worst.mean())
-    )
-    concave_points_worst = st.sidebar.slider(
-        "Concave points (worst)", float(data['concave points_worst'].min()), float(data['concave points_worst'].max()),
-        float(data['concave points_worst'].mean())
-    )
-    symmetry_worst = st.sidebar.slider(
-        "Symmetry (worst)", float(data.symmetry_worst.min()), float(data.symmetry_worst.max()),
-        float(data.symmetry_worst.mean())
-    )
-    fractal_dimension_worst = st.sidebar.slider(
-        "Fractal dimension (worst)", float(data.fractal_dimension_worst.min()),
-        float(data.fractal_dimension_worst.max()), float(data.fractal_dimension_worst.mean())
-    )
+    slider_labels = [("Radius (mean)", "radius_mean"), ("Texture (mean)", "texture_mean"),
+                     ("Perimeter (mean)", "perimeter_mean"), ("Area (mean)", "area_mean"),
+                     ("Smoothness (mean)", "smoothness_mean"), ("Compactness (mean)", "compactness_mean"),
+                     ("Concavity (mean)", "concavity_mean"), ("Concave points (mean)", "concave points_mean"),
+                     ("Symmetry (mean)", "symmetry_mean"), ("Fractal dimension (mean)", "fractal_dimension_mean"),
+                     ("Radius (se)", "radius_se"), ("Texture (se)", "texture_se"), ("Perimeter (se)", "perimeter_se"),
+                     ("Area (se)", "area_se"), ("Smoothness (se)", "smoothness_se"),
+                     ("Compactness (se)", "compactness_se"), ("Concavity (se)", "concavity_se"),
+                     ("Concave points (se)", "concave points_se"), ("Symmetry (se)", "symmetry_se"),
+                     ("Fractal dimension (se)", "fractal_dimension_se"), ("Radius (worst)", "radius_worst"),
+                     ("Texture (worst)", "texture_worst"), ("Perimeter (worst)", "perimeter_worst"),
+                     ("Area (worst)", "area_worst"), ("Smoothness (worst)", "smoothness_worst"),
+                     ("Compactness (worst)", "compactness_worst"), ("Concavity (worst)", "concavity_worst"),
+                     ("Concave points (worst)", "concave points_worst"), ("Symmetry (worst)", "symmetry_worst"),
+                     ("Fractal dimension (worst)", "fractal_dimension_worst")]
 
-    input_data = {
-        'radius_mean': radius,
-        'texture_mean': texture,
-        'perimeter_mean': perimeter,
-        'area_mean': area,
-        'smoothness_mean': smoothness,
-        'compactness_mean': compactness,
-        'concavity_mean': concavity,
-        'concave points_mean': concave_points,
-        'symmetry_mean': symmetry,
-        'fractal_dimension_mean': fractal_dimension,
-        'radius_se': radius_se,
-        'texture_se': texture_se,
-        'perimeter_se': perimeter_se,
-        'area_se': area_se,
-        'smoothness_se': smoothness_se,
-        'compactness_se': compactness_se,
-        'concavity_se': concavity_se,
-        'concave points_se': concave_points_se,
-        'symmetry_se': symmetry_se,
-        'fractal_dimension_se': fractal_dimension_se,
-        'radius_worst': radius_worst,
-        'texture_worst': texture_worst,
-        'perimeter_worst': perimeter_worst,
-        'area_worst': area_worst,
-        'smoothness_worst': smoothness_worst,
-        'compactness_worst': compactness_worst,
-        'concavity_worst': concavity_worst,
-        'concave points_worst': concave_points_worst,
-        'symmetry_worst': symmetry_worst,
-        'fractal_dimension_worst': fractal_dimension_worst
-    }
+    input_data = {}
+
+    for label, col in slider_labels:
+        input_data[col] = st.sidebar.slider(
+            label, float(data[col].min()), float(data[col].max()), float(data[col].mean())
+            )
 
     return input_data
 
@@ -338,6 +211,7 @@ def create_app(data):
         st.write(
             "This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis."
             )
+
 
 def main():
     # EDA
